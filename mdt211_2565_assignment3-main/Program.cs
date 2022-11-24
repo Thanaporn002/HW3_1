bd6aa2c82ed7c4e8e7ef751933d6a0b6a9186158
Thanaporn002 <110844116+Thanaporn002@users.noreply.github.com>
2022-11-25 00:00:43 +0700

    Add files via upload

---
+++ mdt211_2565_assignment3-main/Program.cs
@@ -0,0 +1,37 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+         CircularLinkedList<char> PML = new CircularLinkedList<char>(); 
+
+        while (true)
+        {
+            int range = PML.GetLength(); 
+            Console.WriteLine("Input your flower (J/G/O/R)");
+            char SelectFlower = char.Parse(Console.ReadLine());
+
+            if (SelectFlower != 'J' && SelectFlower != 'G' && SelectFlower != 'O' && SelectFlower != 'R' )
+            {
+                break;
+            }
+            else if  (SelectFlower == 'R')
+            {
+                if (PML.Get(range-1)== 'R' && PML.Get(range-2) == 'G') 
+                {
+                    Console.WriteLine("Invalid pattern.");
+                }
+            }
+            else if (SelectFlower == 'G')
+            {
+                if (PML.Get(range-1)== 'R' && PML.Get(range-2)) 
+                {
+                    Console.WriteLine("Invalid pattern.");
+                }
+            }
+            else
+            {
+                PML.Add(SelectFlower);
+            }
+        }
+    }
+}
